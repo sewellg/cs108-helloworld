f39c38605dac913423b6a474cef32ae68737d397
Grace Sewel <gsewell@bu.edu>
2022-04-03 21:31:35 -0400

    added pillow to pipenv

--- quotes/views.py
+++ quotes/views.py
@@ -4,6 +4,8 @@ from .models import Quote, Person
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 import random
 from .forms import CreateQuoteForm, UpdateQuoteForm
+from django.views.generic.edit import DeleteView  
+from django.urls import reverse 
 
 class HomePageView(ListView):
     '''Create a subclass of ListView to display all quotes.'''
@@ -52,3 +54,22 @@ class UpdateQuoteView(UpdateView):
     model = Quote # which model to create
     form_class = UpdateQuoteForm # which form to use to create the Quote
     template_name = "quotes/create_quote_form.html" # delegate the display to this template
+
+class DeleteQuoteView(DeleteView):
+    '''A view to delete a quote and remove it from the database.'''
+
+    template_name = "quotes/delete_quote.html"
+    queryset = Quote.objects.all()
+
+    def get_success_url(self):
+        '''Return a the URL to which we should be directed after the delete.'''
+
+        # get the pk for this quote
+        pk = self.kwargs.get('pk')
+        quote = Quote.objects.filter(pk=pk).first() # get one object from QuerySet
+        
+        # find the person associated with the quote
+        person = quote.person
+
+        # reverse to show the person page
+        return reverse('person', kwargs={'pk':person.pk})
